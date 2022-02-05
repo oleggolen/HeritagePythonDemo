4aef8d1e730d2d921a10664c192530f4b04e9339
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2022-02-05 14:11:11 +0300

    Add heritage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 class Employee:
-    __employeeType = 'Employee'
 
     def __init__(self, firstname, familyname, middleinitial, hirigdate, department):
         self.__firstname = firstname
@@ -26,61 +25,36 @@ class Employee:
     def getdepartment(self):
         return self.__department
 
-    def getemployeetype(self):
-        return self.__employeeType
+    def printMinInfo(self):
+        return self.__firstname + ' ' + self.__familyname + ' ' + self.__middleinitial
 
 
-class Manager:
+class Manager(Employee):
     __team = []
-    __employeeType = "Manager"
 
     def __init__(self, firstname, familyname, middleinitial, hirigdate, department, level):
-        self.__firstname = firstname
-        self.__familyname = familyname
-        self.__middleinitial = middleinitial
-        self.__hiringdate = hirigdate
-        self.__department = department
+        super().__init__(firstname, familyname, middleinitial, hirigdate, department)
         self.__level = level
 
     def __str__(self):
-        return 'Firstname: ' + self.__firstname + '\nLastname:' + self.__familyname + "\nMiddlename:" + self.__middleinitial + '\nHiring date:' + self.__hiringdate + '\nDepartment:' + self.__department + "\nLevel: " + str(self.__level)
-
-    def getfirstname(self):
-        return self.__firstname
-
-    def getlsatname(self):
-        return self.__familyname
-
-    def getmiddleinitial(self):
-        return self.__middleinitial
-
-    def gethiringdate(self):
-        return self.__hiringdate
-
-    def getdepartment(self):
-        return self.__department
+        return super(Manager, self).__str__()+'\nLevel: ' + str(self.__level)
 
     def getlevel(self):
+        #super(Manager, self).__middleinitial =
+
         return self.__level
 
     def getteam(self):
         return self.__team
 
-    def getemployeetype(self):
-        return self.__employeeType
+    def printMinInfo(self):
+        return str(self.__level) + ' ' + str(len(self.__team))
 
 
 def printemployee(employe):
-    print('Firstname', employe.getfirstname())
-    print('Lastname:', employe.getlsatname())
-    print('Middlename', employe.getmiddleinitial())
-    print('Hiring date', employe.gethiringdate())
-    print('Department:', employe.getdepartment())
-    if employe.getemployeetype() == 'Manager':
-        print('Level', employe.getlevel())
-        for member in employe.getteam():
-            printemployee(member)
-            print()
+    print(employe)
+    print()
+    print(employe.printMinInfo())
 
 
 employee = Employee('Oleg', 'Golenischev', 'K', '19.08.1994', 'Teachers')
@@ -88,7 +62,12 @@ print(employee)
 print()
 manager = Manager('Ivan', 'Netreba', 'I', '20.12.20', 'Managers', 3)
 print(manager)
+print(manager.getlevel())
 print()
 printemployee(employee)
 print()
 printemployee(manager)
+print(employee.printMinInfo())
+print()
+print(manager.printMinInfo())
+
